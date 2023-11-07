7bd5b4654721881044b9f94b160e216da99b103b
Harsh Deep <hdeep61034@gmail.com>
2023-11-07 22:40:57 +0530

    express server setted up

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const { connectToDb } = require("./connect")
 
-connectToDb("mongodb://27017/shortid")
+connectToDb("mongodb://127.0.0.1:27017/shortid")
 	.then((res) => console.log("Mongo DB Connected"))
 	.catch((err) => console.log("MongoDB ERROR: ", err));
+
+app.get("/", (req, res) => {
+	res.end("HELLO")
+})
+app.listen(8080, () => console.log("Server Listening at PORT 8080 "))
