1a4d00743a068367605e4dc1e6953c4712eb2ba2
Harsh Deep <hdeep61034@gmail.com>
2023-11-18 21:18:50 +0530

    added password encryption using bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,13 @@ const User = require("../models/user");
 require('dotenv').config();
 const secretKey = process.env.JWT_SECRET_KEY;
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 async function handleCreateNewUser(req, res) {
 	const { name, email, password } = req.body;
-	await User.create({ name, email, password });
-	const accessToken = jwt.sign({ name, email, password }, secretKey);
+	const hashedPassword = await bcrypt.hash(password, 10);
+	await User.create({ name, email, password: hashedPassword });
+	const accessToken = jwt.sign({ name, email }, secretKey);
 	res.cookie('accessToken', accessToken, { httpOnly: true });
 	res.redirect("http://localhost:8080/");
 }
@@ -15,8 +17,9 @@ async function handleLoginUser(req, res) {
 	const { email, password } = req.body;
 	const user = await User.findOne({ email });
 	if (!user) return res.status(400).render("login", { error: "User doesn't exists!" });
-	if (user.password == password) {
-		const accessToken = jwt.sign({ name: user.name, email, password }, secretKey);
+	const passwordMatch = await bcrypt.compare(password, user.password);
+	if (passwordMatch) {
+		const accessToken = jwt.sign({ name: user.name, email }, secretKey);
 		res.cookie('accessToken', accessToken, { httpOnly: true });
 		res.redirect("http://localhost:8080/");
 	}
