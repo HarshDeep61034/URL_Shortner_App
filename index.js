b4a81683304c2e29c7040889717c2fbc7517b9a0
Harsh Deep <hdeep61034@gmail.com>
2023-11-24 12:45:54 +0530

    server debugging

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = 8080;
-const staticRouter = require("./routes/static");
-const userRouter = require("./routes/user");
-const urlRouter = require("./routes/url");
-const { connectToDb } = require("./connect");
-const authenticateToken = require("./auth");
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const path = require("path");
-require('dotenv').config();
-const user = process.env.MONGO_DB_ADMIN;
-const password = process.env.MONGO_DB_PASSWORD;
 
-// Connecting to MongoDB via Mongoose
-connectToDb(`mongodb+srv://${user}:${password}@cluster0.afbycsy.mongodb.net/`)
-	.then((res) => console.log("Mongo DB Connected"))
-	.catch((err) => console.log("MongoDB ERROR: ", err));
+app.get("/", (req, res) => {
+	res.end("<h1>Server Working!!</h1>")
+})
 
-// Setting ejs as template engine to render SSR pages
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
-
-// MIDDLEWARES
-app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
-
-// MIDDLEWARES - with custom routers
-app.use("/user", userRouter); // for user register,login,logout.
-app.use("/url", authenticateToken, urlRouter); // for shortUrl creation & fetch analytics.
-app.use("/", authenticateToken, staticRouter); // for Static Pages rendering & url redirection.
-
-
-
-app.listen(PORT, () => console.log(`Server Listening at PORT: ${PORT}`));
+app.listen(2000, (err) => console.log("Listening Server"));
